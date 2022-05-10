678c05389bdff5380dacaf33128b51fc19b3fae0
Michael Cullan <mjcullan@gmail.com>
2022-05-10 15:27:10 -0400

    sveltekit initial commit

---
+++ build/_app/chunks/dark-5389f6b2.js
@@ -0,0 +1,1 @@
+import{w as e}from"./index-79aa9995.js";const o=!1;var a;const l=(a=window.localStorage.getItem("dark"))!=null?a:o,r=e(l);r.subscribe(t=>{window.localStorage.setItem("dark",t)});export{r as d};
